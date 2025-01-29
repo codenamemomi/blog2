fd8b1358741b1d1ffc3ea697d35cec15445e3b56
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-01-29 14:28:12 +0100

    edited urls

--- spotlightcentral/urls.py
+++ spotlightcentral/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('news/', views.news_list, name='news_list'),  # New URL pattern for news list
     path('feed/', LatestPostFeed(), name='post_feed'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     # ... other URL patterns
 ]
